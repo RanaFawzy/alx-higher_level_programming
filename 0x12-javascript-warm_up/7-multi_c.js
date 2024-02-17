5e91b99acfba1d2cecbf9f480f07f761b860ba69
RanaFawzy <rororona91@gmail.com>
2024-02-17 13:31:54 -0800

    T

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+
+let printXt = parseInt(process.argv[2]);
+if (isNaN(printXt) || process.argv[2] === undefined) {
+  console.log('Missing number of occurences');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+  while (printXt > 0) {
     console.log('C is fun');
-    i++;
+    printXt--;
   }
 }
