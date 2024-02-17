c90d980773cbd51fe1c60447ada81b2129d14002
RanaFawzy <rororona91@gmail.com>
2024-02-17 13:54:19 -0800

    T

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,18 @@
-#!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+#!/usr/bin/node
 
+function factorial (a) {
+  if (a <= 0) {
+    return 0;
+  } else if (a === 1) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
-        print("Not found")
+const myInt = parseInt(process.argv[2]);
+if (isNaN(myInt)) {
+  console.log('1');
+} else {
+  console.log(factorial(myInt));
+}
