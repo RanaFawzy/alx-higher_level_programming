6e230be7c7567cb1dfc79e70c611572de5807dcf
RanaFawzy <rororona91@gmail.com>
2024-02-19 13:06:44 -0800

    T

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""
-This script defines a City class
-to work with MySQLAlchemy ORM.
-"""
-
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
 
-class City(Base):
-    """City class
-
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-        state_id (int): The state the city belongs to
-
-    """
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+if _name_ == "_main_":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
