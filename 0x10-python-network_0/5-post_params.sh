fd4441a79b5966c3b2031bf631f42ae507a976fa
RanaFawzy <rororona91@gmail.com>
2024-02-29 18:59:13 -0800

    python-network_0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A variable subject must be sent with the value I will always be here for PLD
-curl -sX POST -d"email=test@gmail.com&subject=I will always be here for PLD"$1
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d"email=test@gmail.com&subject=I will always be here for PLD"
